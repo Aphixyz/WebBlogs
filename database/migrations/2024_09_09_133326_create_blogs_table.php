2b60f693ec9dfb9c8547dacce2fa9ff6b775a3db
TanaratSudjai <650112418012@gmail.com>
2024-09-09 20:45:39 +0700

    success migration

---
+++ database/migrations/2024_09_09_133326_create_blogs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('content');
+            $table->integer('connection');
+            $table->string('image');
+
+            $table->unsignedBigInteger("category_id")->nullable();
+            ;
+            $table->unsignedBigInteger("user_id")->nullable();
+            ;
+
+            $table->timestamps();
+
+            //ref
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('set null');
+            $table->foreign('category_id')->references('id')->on('category')->onDelete('set null');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
