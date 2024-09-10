3af938ce9f9268e114b776cde8a5d09685ca608d
Aphixyz <650112418030@bru.ac.th>
2024-09-10 16:49:11 +0700

    modify-adtiveView

--- resources/views/admin/active.blade.php
+++ resources/views/admin/active.blade.php
@@ -17,6 +17,22 @@
     <a href="{{ route('admin.getblog') }}">get blog</a>
     <a href="{{ route('admin.formaddcategory') }}">add category</a>
     <a href="{{ route('admin.getCategory') }}">get category</a>
+
+    <form action="">
+        <h1>
+            @if (session('message'))
+                <p>Dashboard {{ session('message') }}!</p>
+            @endif
+        </h1>
+        <div class="getblog">
+            <a href="{{ route('admin.getblog') }}">get blog</a>
+        </div>
+        <div class="addCate">
+            <a href="{{ route('admin.formaddcategory') }}">add category</a>
+        </div>
+        <div class="getCate">
+            <a href="{{ route('admin.getCategory') }}">get category</a>
+        </div>
 </body>
 
 </html>
