7f06ab7fbd0dbb746473a9ba85053d0c7614a359
Aphixyz <650112418030@bru.ac.th>
2024-09-10 15:03:43 +0700

    modifyFormAdd
    noSuccess

--- resources/views/people/addblog.blade.php
+++ resources/views/people/addblog.blade.php
@@ -12,11 +12,11 @@
     <h2>Blogs</h2>
     <form action="{{ route('people.add') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <input type="text" name="name" id="name" required><br><br>
-        <input type="text" name="description" id="description" required><br><br>
-        <input type="text" name="content" id="content" required><br><br>
-        <input type="number" name="connection" id="connection" required><br><br>
-        <input type="file" name="image" id="image" accept="image/*"><br><br>
+        <input type="text" name="name" id="name" required>ชื่อบทความ<br><br>
+        <input type="text" name="description" id="description" required>คำอธิบาย<br><br>
+        <input type="text" name="content" id="content" required>เนื้อหาบทความ<br><br>
+        <input type="number" name="connection" id="connection" required>เกี่ยวข้องของบทความ<br><br>
+        <input type="file" name="image" id="image" accept="image/*"><br>รูปภาพของบทความ<br>
         <label for="category_id">Select a category:</label>
         <select name="category_id" id="category_id" required>
             <option value="" disabled selected>Select a category</option>
