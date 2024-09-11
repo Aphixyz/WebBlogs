d12663f0bc0390f7966c8d12d654c7b8e5d8ce02
TanaratSudjai <650112418012@gmail.com>
2024-09-11 15:58:27 +0700

    commit admin addblog

--- app/Http/Controllers/admincontroller.php
+++ app/Http/Controllers/admincontroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\blog;
 use App\Models\Category;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class admincontroller extends Controller
 {
     public function active()
@@ -18,18 +18,62 @@ class admincontroller extends Controller
     }
 
 
-    
-    public function getBlogForAdmin(){
+    public function adminCreateblog()
+    {
+        $categories = Category::all();
+
+        return view('admin.addblogadmin', compact('categories'));
+    }
+
+
+    public function CreateBolg(Request $req)
+    {
+        $validatedData = $req->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'content' => 'required|string|max:255',
+            'image' => 'nullable|image|mimes:jpg,jpeg,png,gif|max:2048',
+            'category_id' => 'required|exists:categories,id',
+        ]);
+        $peopleId = Auth::id();
+
+
+        $imagePath = null;
+        if ($req->hasFile('image')) {
+            $image = $req->file('image');
+            if ($image->isValid()) {
+                $imagePath = $image->move(public_path('images'), $image->getClientOriginalName());
+            } else {
+                return redirect()->back()->withErrors(['image' => 'Image upload failed.'])->withInput();
+            }
+        }
+
+        $blog = new Blog();
+        $blog->name = $validatedData['name'];
+        $blog->description = $validatedData['description'];
+        $blog->content = $validatedData['content'];
+        $blog->connection = 0;
+        $blog->image = $imagePath ? 'images/' . $image->getClientOriginalName() : null;
+        $blog->user_id = $peopleId;
+        $blog->category_id = $validatedData['category_id'];
+        $blog->save();
+        return redirect()->back()->with('success', 'Blog created successfully!');
+    }
+
+    public function getBlogForAdmin()
+    {
         $datablog = blog::all();
-        return $datablog ; 
+        return $datablog;
     }
 
-    public function getCategory(){
+    public function getCategory()
+    {
         $category = Category::all();
-        return $category ; 
+        return $category;
     }
 
-    public function createCategory(Request $req){
+    public function createCategory(Request $req)
+    {
         $validatedData = $req->validate([
             'name' => 'required|string|max:255',
         ]);
